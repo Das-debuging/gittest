b1dba2482db2e22a7378b7a0422c1fb077fe8574
Das-debuging <sharukh.daselectronics@gmail.com>
2024-09-20 13:37:33 +0530

    firt c

---
+++ 1.c
@@ -0,0 +1,6 @@
+//2.c
+#include <stdio.h>
+#include "2.c"
+void fun1() {
+printf(we are in function fun1() from 1.c\n")
+}
---
+++ 2.c
@@ -0,0 +1,6 @@
+//2.c
+#include <stdio.h>
+#include "2.c"
+void fun2() {
+printf(we are in function fun2 from 2.c\n");
+}
